ff116288ea86904b4816dba7425da78ef39df29d
James <79054685+henry-js@users.noreply.github.com>
2023-02-09 15:17:56 +0000

    6.Linq

--- 5.TruckRental/Program.cs
+++ 5.TruckRental/Program.cs
@@ -1,48 +1,60 @@
-﻿int rentForMins = 310;
+﻿int rentForMins = 190;
 var fromDate = DateTime.UtcNow;
 var toDate = fromDate.AddMinutes(rentForMins);
-float cost = CalculateRentalCost(fromDate, toDate);
+decimal cost = CalculateRentalCost(fromDate, toDate);
 
-Console.WriteLine($"Cost to rent truck for {rentForMins} minutes: £{cost}");
+Console.WriteLine($"Cost to rent truck for {rentForMins} minutes: {cost:C}");
 
-float CalculateRentalCost(DateTime From, DateTime To)
+decimal CalculateRentalCost(DateTime From, DateTime To)
 {
+    decimal finalCost = 0;
     var requestedRentalTime = To - From;
-    var requestedRentalTimeMins = requestedRentalTime.TotalMinutes;
-    var standardPeriodMins = RentalDefaults.standardPeriodMins;
-    var initialPeriodCost = RentalDefaults.initialPeriodCost;
-    var initialPeriodMins = RentalDefaults.initialPeriodMins;
-    var roundUpAfter = RentalDefaults.roundUpAfter;
-    var standardPeriodCost = RentalDefaults.standardPeriodCost;
-    bool roundUp;
-    float finalCost = default;
-    double actualRentalTime;
-    var leftoverTime = requestedRentalTimeMins % standardPeriodMins;
-    roundUp = leftoverTime >= roundUpAfter;
+    var remainingTime = requestedRentalTime.Subtract(new TimeSpan(0, RentalDefaults.initialPeriodMins, 0));
+    finalCost += RentalDefaults.initialPeriodCost;
+    if (remainingTime.TotalMinutes <= 0)
+    {
+        return finalCost;
+    }
+    while (remainingTime.TotalMinutes > 0)
+    {
+        if (remainingTime.TotalMinutes >= 10)
+        {
+            remainingTime = remainingTime.Subtract(new TimeSpan(0, RentalDefaults.standardPeriodMins, 0));
+            finalCost += RentalDefaults.standardPeriodCost;
+        }
+        else
+        {
+            return finalCost;
+        }
 
-    if (roundUp)
-    {
-        actualRentalTime = requestedRentalTimeMins - leftoverTime + standardPeriodMins;
     }
-    else
-    {
-        actualRentalTime = requestedRentalTimeMins;
-    }
-    finalCost += initialPeriodCost;
-    actualRentalTime -= initialPeriodMins;
-    while (actualRentalTime != 0)
-    {
-        actualRentalTime -= standardPeriodMins;
-        finalCost += standardPeriodCost;
-    }
+    // finalCost
+    // var standardPeriodMins = RentalDefaults.standardPeriodMins;
+    // var initialPeriodCost = RentalDefaults.initialPeriodCost;
+    // var initialPeriodMins = RentalDefaults.initialPeriodMins;
+    // var roundUpAfter = RentalDefaults.roundUpAfter;
+    // var standardPeriodCost = RentalDefaults.standardPeriodCost;
+    // bool roundUp;
+    // double actualRentalTime;
+    // var leftoverTime = requestedRentalTime.Minutes % standardPeriodMins;
+    // roundUp = leftoverTime >= roundUpAfter;
+
+    // actualRentalTime = roundUp ? requestedRentalTime.Minutes - leftoverTime + standardPeriodMins : requestedRentalTime.Minutes;
+    // finalCost += initialPeriodCost;
+    // actualRentalTime -= initialPeriodMins;
+    // while (actualRentalTime != 0)
+    // {
+    //     actualRentalTime -= standardPeriodMins;
+    //     finalCost += standardPeriodCost;
+    // }
 
     return finalCost;
 }
 public static class RentalDefaults
 {
-    public static readonly int initialPeriodCost = 25;
+    public static readonly decimal initialPeriodCost = 25M;
+    public static readonly decimal standardPeriodCost = 50M;
     public static readonly int initialPeriodMins = 60;
-    public static readonly int standardPeriodCost = 50;
     public static readonly int standardPeriodMins = 60;
     public static readonly int roundUpAfter = 10;
 }
---
+++ 6.Linq/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Text;
+
+var unorderedString = "Hello World";
+
+var result = unorderedString.Select(x => x.ToString()).Order();
+var strings = result.Where(a => !a.Equals(" "));
+var grouped = from letter in strings
+              group letter by letter.ToLowerInvariant() into newList
+              orderby newList.Count() descending
+              select (new { Letter = newList.Key, Count = newList.Count() });
+
+Console.WriteLine($"Started with: {unorderedString}");
+foreach (var val in grouped)
+{
+    Console.WriteLine($"Letter: {val.Letter}, Count: {val.Count}");
+    Console.WriteLine($"");
+}
